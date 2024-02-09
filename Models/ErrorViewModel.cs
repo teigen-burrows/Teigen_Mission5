ad17efd80945b9cd313bc17c7eca2005443c555e
teigen burrows <teigenburrows@gmail.com>
2024-02-09 13:57:58 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Teigen_Mission5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculateButton').click(function () {
+        var hours = parseInt($('#hours').val());
+        if (isNaN(hours) || hours <= 0) {
+            alert('Please enter a valid positive number for hours.');
+            return;
+        }
+
+        var hourlyRate = parseFloat($('#hourlyRate').val());
+        var total = hours * hourlyRate;
+        $('#total').val(total.toFixed(2));
+    });
+});
+
